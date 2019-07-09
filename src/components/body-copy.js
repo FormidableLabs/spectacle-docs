052e445e806f5bea95f84c5ab391a1640d73a63e
Narin Luangrath <narin.luangrath@gmail.com>
2019-07-09 09:18:28 -0700

    Update more oss section

--- src/components/body-copy.js
+++ src/components/body-copy.js
@@ -5,10 +5,13 @@ export const BodyCopy = styled.p`
   font-size: 1.2rem;
   line-height: 1.5;
   color: #4e4e4e;
-  text-align: center !important;
+  text-align: center;
   margin: 0 auto;
   width: 100%;
   @media (min-width: 768px) {
     font-size: 1.5rem;
   }
+  @media (min-width: 768px) {
+    text-align: center;
+  }  
 `;
