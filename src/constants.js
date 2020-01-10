92dc9a8b0347ebd123b87cca605935819a3a0dcd
Kylie Stewart <kylie.stewart@formidable.com>
2020-01-10 15:53:52 -0700

    Deprecate project (#95)

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-  docsTitle: "SPECTACLE",
-  githubIssues: "https://www.github.com/FormidableLabs/spectacle/issues",
-  github: "https://www.github.com/FormidableLabs/spectacle",
-  color: "#eeeeee"
-};
-
-export default constants;
